dba1a351cedcacd29fba6ddb20b9f3657143edeb
Eduardo Whitehurst <119756830+eduardo-whitehurst@users.noreply.github.com>
2023-10-12 10:02:46 -0300

    Finalizando o Backend

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const pontos = require("./src/Routes");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(pontos);
 
 const porta = process.env.API_PORT;
 
