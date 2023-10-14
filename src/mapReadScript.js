e0375ca2eb42dda451f58e9200eea13d359b2817
Eduardo Whitehurst <119756830+eduardo-whitehurst@users.noreply.github.com>
2023-10-13 21:19:29 -0300

    Editando a estrutura da página de leitura e inserção de ocorrências.

---
+++ src/mapReadScript.js
@@ -0,0 +1,7 @@
+function abrirForm() {
+    document.querySelector(".form").style.display = "block";
+  }  
+
+  function fecharForm() {
+    document.querySelector(".form").style.display = "none";
+  }  
